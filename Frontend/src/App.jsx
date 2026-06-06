ab5330293208c7f8a1924d4811db410bf50eca34
Shaikh Mubashir <shaikhmubashirahmed123@gmail.com>
2026-06-06 15:07:52 +0530

    feat: implement routing with Navbar and Home/Product components, remove unused CSS

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Product from "./Components/Product";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/product" element={<Product/>}/>
+      </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import {Link, Links} from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav className="">
+    <>
+    
+    <nav className="bg-gray-800 text-white p-6 flex justify-start gap-5 items-center">
+        <div className="text-2xl font-bold">
+          <div><h1>E-Commerce</h1></div>
+        </div>
+        <div>
+          <ul className='flex text-xl gap-5'>
+            <Link to="/">Home</Link>
+            <Link to="/product">Add Product</Link>
+          </ul>
+        </div>
+        
 
     </nav>
+    </>
   )
 }
 
