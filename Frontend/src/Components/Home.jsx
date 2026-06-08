7678100ce63be24773546f1c77535aef50eb1513
Shaikh Mubashir <shaikhmubashirahmed123@gmail.com>
2026-06-08 17:06:41 +0530

    Refactor Home component layout and enhance product display with additional details

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -30,15 +30,19 @@ function Home() {
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-2 px-6 sm:px-0 ">
+      <div className="flex items-center justify-between min-h-screen bg-gray-100 py-2 px-6 sm:px-0 ">
         {products.map((product) => {
           return (
             <div
-              className="bg-white rounded-lg shadow-md p-4 m-4"
+              className="bg-white rounded-lg shadow-md p-4 m-4 flex flex-col items-center justify-center h-64 w-64"
               key={product.id}
             >
-              <h2 className="text-xl font-bold mb-2">{product.name}</h2>
-              <p className="text-gray-600">{product.description}</p>
+              <h2 className="text-xl font-bold mb-2">{product.name.toUpperCase()}</h2>
+              <p className="text-gray-600">{product.brand}</p>
+              <p className="text-gray-600">₹{product.price.toFixed(2)}</p>
+              <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                Add To Cart
+              </button>
             </div>
           );
         })}
