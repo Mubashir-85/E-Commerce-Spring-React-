22ab4a6b5e9603f525e1248246936552469d9228
Shaikh Mubashir <shaikhmubashirahmed123@gmail.com>
2026-06-09 20:48:22 +0530

    Fix typos and improve formatting in AppContext component

--- Frontend/src/Context/Context.jsx
+++ Frontend/src/Context/Context.jsx
@@ -1,51 +1,76 @@
-import axios from 'axios';
-import React, { createContext, useState } from 'react'
+import axios from "axios";
+import React, { createContext, useState } from "react";
 
 const AppContext = createContext({
-        data:[],
-        isError:"",
-        cart:[],
-        addToCart:(product)=>{},
-        removeFromCart:(productId)=>{},
-        refreshData:()=>{},
-        updateStockQuantity:(productId,quantity)=>{}
-    });
+  data: [],
+  isError: "",
+  cart: [],
+  addToCart: (product) => {},
+  removeFromCart: (productId) => {},
+  refreshData: () => {},
+  updateStockQuantity: (productId, quantity) => {},
+});
 
-    export const AppProvider = ({children})=>{
-        const [data,setData] = useState([]);
-        const [isError,setIsError] = useState("");
-        const [cart,setCart] = useState([]);
+export const AppProvider = ({ children }) => {
+  const [data, setData] = useState([]);
+  const [isError, setIsError] = useState("");
+  const [cart, setCart] = useState([]);
 
-        const addToCart = (product)=>{
-            const existingProduct = cart.findIndex((item)=>item.id === product.id);
-            if(existingProduct !== -1){
-                const updatedCart = cart.map((item,index)=>
-                    idex === existingProduct 
-                    ? {...item,quantity:item.quantity + 1}
-                    : item
-                )
-                setCart(updatedCart);
-                localStorage.setItem("cart",JSON.stringify(updateCart));
-            }else{
-                const updatedCart = [...cart,{...product,quantity:1}];
-                setCart(updatedCart);
-                localStorage.setItem("cart",JSON.stringify(updatedCart));
-            }
-        }
-
-        const removeFromCart =(productId)=>{
-            const updatedCart = cart.filter((item)=>item.id !== productId);
-            setCart(updatedCart);
-            localStorage.setItem("cart",JSON.stringify(updatedCart));
-
-        };
-
-        const refershData = async()=>{
-            // try{
-            //     const response = await axios.get()
-            // }
-        }
+  const addToCart = (product) => {
+    const existingProduct = cart.findIndex((item) => item.id === product.id);
+    if (existingProduct !== -1) {
+      const updatedCart = cart.map((item, index) =>
+        idex === existingProduct
+          ? { ...item, quantity: item.quantity + 1 }
+          : item,
+      );
+      setCart(updatedCart);
+      localStorage.setItem("cart", JSON.stringify(updateCart));
+    } else {
+      const updatedCart = [...cart, { ...product, quantity: 1 }];
+      setCart(updatedCart);
+      localStorage.setItem("cart", JSON.stringify(updatedCart));
     }
+  };
 
+  const removeFromCart = (productId) => {
+    const updatedCart = cart.filter((item) => item.id !== productId);
+    setCart(updatedCart);
+    localStorage.setItem("cart", JSON.stringify(updatedCart));
+  };
+
+  const refershData = async () => {
+    try {
+      const response = await axios.get("/products");
+      setData(response.data);
+    } catch (error) {
+      setIsError(error.message);
+    }
+  };
+
+  const clearCart = () => {
+    setData([]);
+  };
+
+  useEffect(() => {
+    refershData();
+  }, []);
+
+  return (
+    <AppContext.Provider
+      value={{
+        data,
+        isError,
+        cart,
+        addToCart,
+        removeFromCart,
+        refreshData,
+        clearCart,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
 
 export default AppContext;
